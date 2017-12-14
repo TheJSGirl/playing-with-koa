c529ac55aeb7b7267b58acaf0dab5fa4f83577ac
Priyanka Negi <prinegi100@gmail.com>
2017-12-14 12:23:01 +0530

    created SeedDB

---
+++ src/notes/tests/SeedDB/index.js
@@ -0,0 +1,30 @@
+// this file will contain the seed data
+const { ObjectID } = require('mongodb');
+const Notes = require('../../model');
+
+// create an id for Notes
+const noteOne = new ObjectID();
+const noteTwo = new ObjectID();
+const noteThree = new ObjectID();
+
+const dummyData = [
+
+  {
+    _id: noteOne,
+    notes: 'dummy data one'
+  },
+  {
+    _id: noteTwo,
+    notes: 'dummy data two'
+  },
+  {
+    _id: noteThree,
+    notes: 'dummy data three'
+  },
+];
+
+const populatedData = (done) => {
+  Notes.remove({}).then(() => Notes.insertMany(dummyData)).then(() => done());
+}
+
+module.exports = { populatedData, dummyData };
