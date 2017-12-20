db4de8b36b96ce534a25b6984fb8531919250d1a
Priyanka Negi <prinegi100@gmail.com>
2017-12-20 09:35:55 +0530

    Update secureUserData.js

--- utils/secureUserData.js
+++ utils/secureUserData.js
@@ -1,5 +1,6 @@
 module.exports = function secureUserData(obj) {
-  const newObj = JSON.parse(JSON.stringify(obj));
+  // const newObj = JSON.parse(JSON.stringify(obj));
+  const newObj = obj.toJSON();
   delete newObj.password;
   delete newObj._v;
   return newObj;
